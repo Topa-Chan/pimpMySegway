c597580aaa4a54f1dd1469f5d9ac0af3313212bc
Topanga <tpatterson@student.neumont.edu>
2018-07-27 08:54:55 -0600

    Added MORE
    Added ui-elements scss file for custom classes and ids
    Added import for ui-elements in main.scss
    Added functions and elements inside of script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,29 @@
-var tireTypes = ['Bike', 'Tractor', 'Normal'];
+//Main Div
+var segwayApp = document.getElementById("segway");
+
+//Buttons
 var colours = ['Gold', 'Red', 'Green', 'Blue', 'Black'];
+colours.name = "Colours";
+var tireTypes = ['Bike', 'Tractor', 'Normal'];
+tireTypes.name = "Tires";
 var engineTypes = ['Electric', 'Petrol', 'Dual'];
+engineTypes.name = "Engines";
 var customAdds = ['Deep Fried', 'Cup Holders', 'Tassels', 'Extra Batteries', 'Horn', 'Basket', 'Bluetooth Speakers', 'Glitter']
+customAdds.name = "Customs";
 
+//Tab Elements 
+var colourTab = document.createElement("div");
+colourTab.setAttribute('class', 'colourTab tab')
+var tireTab = document.createElement("div");
+tireTab.setAttribute('class', 'tireTab tab')
+var engineTab = document.createElement("div");
+engineTab.setAttribute('class', 'engineTab tab');
+var customTab = document.createElement("div");
+customTab.setAttribute('class', 'customTab tab');
+var tabs = [colourTab, tireTab, engineTab, customTab];
+var buttons = [];
 
-
+//Page
 var preBuilts;
 var homePage;
 var confirmPage;
@@ -12,3 +31,75 @@ var customPage;
 var checkoutPage;
 var thankYouPage;
 
+//Building buttons
+
+
+//Build Buttons Function
+function buildButton(item, index, arr){
+  console.log("Array Name: " + arr.name);
+  console.log("button " + item + " at index " + index + ' created.');
+  buttons[index] = document.createElement('div');
+  buttons[index].textContent = item;
+  // buttons[index].setAttribute('class', 'btn');
+  if (arr.name === "Colours") {
+    buttons[index].setAttribute('class', 'btn colour');
+    colourTab.appendChild(buttons[index]);
+  }
+  if (arr.name === "Tires") {
+    buttons[index].setAttribute('class', 'btn tire');
+    tireTab.appendChild(buttons[index]);
+  }
+  if (arr.name === "Engines") {
+    buttons[index].setAttribute('class', 'btn engine');
+    engineTab.appendChild(buttons[index]);
+  } 
+  if (arr.name === "Customs") {
+    buttons[index].setAttribute('class', 'btn custom');
+    customTab.appendChild(buttons[index]);
+  }
+  // container.appendChild(buttons[index]);
+  buttons[index].addEventListener('click', btnClicked);
+}
+
+//Button Clicked Evt
+function btnClicked(evt) {
+  console.log(evt.target.innerText + " clicked");
+  // evt.target.style.backgroundColor = "#21f";
+  var preSplitName = evt.target.attributes[0].value;
+  console.log("Pre Split: " + preSplitName);
+  var splitName = preSplitName.split(" ");
+  console.log("Split Name: " + splitName);
+  var name = splitName[1];
+  console.log("Name: " + name)
+  if (name === "colour") {
+    console.log("Colour");
+    //crustBtnNames.forEach(checkActive, evt);
+    //evt.target.classList.add("active");
+  } 
+  if (name === "tire") {
+    console.log("Tire");
+    //cheeseBtnNames.forEach(checkActive, evt);
+    //evt.target.classList.add("active");
+  }
+  if (name === "engine") {
+    console.log("Engine")
+    //sauceBtnNames.forEach(checkActive, evt);
+    //evt.target.classList.add("active");
+  }
+  if (name === "custom") {
+    console.log("Custom")
+    //toppingsActive(evt);
+  }
+}
+
+//Building Pages
+buildCustomPage(customPage, tabs);
+
+function buildCustomPage(page, tab) {
+  for (var i = 0; i < tab.length; i++) {
+    console.log("Tab: " + tab[i]);
+  }
+  page = document.createElement("div");
+  page.appendChild()
+  segwayApp.appendChild(page);
+}
